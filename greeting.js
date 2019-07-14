b5d3037b0c2820cac50e12007688f08ff3e96a81
rhxfkg <51471056+rhxfkg@users.noreply.github.com>
2019-07-14 22:38:04 +0900

    Add files via upload

---
+++ greeting.js
@@ -0,0 +1,3 @@
+﻿function welcome(){
+    return 'Hello world';
+}
---
+++ node.demo.js
@@ -0,0 +1,3 @@
+﻿var circle = require('./node.circle.js');
+console.log( 'The area of a circle of radius 4 is '
+           + circle.area(4));
